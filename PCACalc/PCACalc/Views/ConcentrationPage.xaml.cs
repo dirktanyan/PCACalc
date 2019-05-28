1373cf66c68c24f74d48f6ffc6404476ea0228be
Joshua Eudy <josh@familypharmacy.us>
2019-05-28 08:48:39 -0400

    Removed Calc button. Calc now performed after fields are updated.

--- PCACalc/PCACalc/Views/ConcentrationPage.xaml.cs
+++ PCACalc/PCACalc/Views/ConcentrationPage.xaml.cs
@@ -20,6 +20,22 @@ namespace PCACalc.Views
             InitializeComponent();
         }
 
+        private bool checkEntries()
+        {
+            if(BasalEntry.Text != null)
+            {
+                if(BolusEntry.Text != null)
+                {
+                    if(BolusMinEntry.Text != null)
+                    {
+                        if(BagVolumeEntry.Text != null)
+                        {
+                            return true;
+                        } else { return false; }
+                    } else { return false; }
+                } else { return false; }
+            } else { return false; }
+        }
         private void CalcConcentration(object sender, EventArgs e)
         {
             double totalPerHour;
@@ -28,37 +44,67 @@ namespace PCACalc.Views
             double bagVolume;
             double suggestedConcentration;
 
-            totalPerHour = Calculations.CalcTPH(double.Parse(BasalEntry.Text), double.Parse(BolusEntry.Text), double.Parse(BolusMinEntry.Text));
-            TPHEntry.Text = totalPerHour.ToString() + unitOfMeasure;
+            if (checkEntries() == true)
+            {
+                try
+                {
+                    totalPerHour = Calculations.CalcTPH(double.Parse(BasalEntry.Text), double.Parse(BolusEntry.Text), double.Parse(BolusMinEntry.Text));
+                    TPHEntry.Text = totalPerHour.ToString() + unitOfMeasure;
 
-            totalPerDay = totalPerHour * 24;
-            TPDEntry.Text = totalPerDay.ToString() + unitOfMeasure;
+                    totalPerDay = totalPerHour * 24;
+                    TPDEntry.Text = totalPerDay.ToString() + unitOfMeasure;
 
-            totalPerDS = totalPerDay * StepDays.Value;
-            TP5DEntry.Text = totalPerDS.ToString() + unitOfMeasure;
+                    totalPerDS = totalPerDay * StepDays.Value;
+                    TP5DEntry.Text = totalPerDS.ToString() + unitOfMeasure;
 
-            bagVolume = double.Parse(BagVolumeEntry.Text);
-            suggestedConcentration = Math.Round(totalPerDS / bagVolume, 0);
+                    bagVolume = double.Parse(BagVolumeEntry.Text);
+                    suggestedConcentration = Math.Round(totalPerDS / bagVolume, 0);
 
 
-            SugConcEntry.Text = suggestedConcentration.ToString() + unitOfMeasure;
+                    SugConcEntry.Text = suggestedConcentration.ToString() + unitOfMeasure;
+                }
+                catch
+                {
+                    //Don't do anything, just don't crash
+                }
+            }
         }
 
         private void selectUOM(object sender, ToggledEventArgs e)
         {
-            if(SwitchMG.IsToggled == true)
+            if (sender.Equals(SwitchMG))
             {
-                SwitchMCG.IsToggled = false;
-                unitOfMeasure = "mg";
-                BasalMeasure.Text = "mg";
-                BolusMeasure.Text = "mg";
+                if (SwitchMG.IsToggled == true)
+                {
+                    SwitchMCG.IsToggled = false;
+                    unitOfMeasure = "mg";
+                    BasalMeasure.Text = "mg";
+                    BolusMeasure.Text = "mg";
+                }
+                else
+                {
+                    SwitchMCG.IsToggled = true;
+                    unitOfMeasure = "mcg";
+                    BasalMeasure.Text = "mcg";
+                    BolusMeasure.Text = "mcg";
+                }
             }
             else
             {
-                SwitchMCG.IsToggled = true;
-                unitOfMeasure = "mcg";
-                BasalMeasure.Text = "mcg";
-                BolusMeasure.Text = "mcg";
+                if(SwitchMCG.IsToggled == true)
+                {
+                    SwitchMG.IsToggled = false;
+                    unitOfMeasure = "mcg";
+                    BasalMeasure.Text = "mcg";
+                    BolusMeasure.Text = "mcg";
+                }
+                else
+                {
+                    SwitchMG.IsToggled = true;
+                    unitOfMeasure = "mg";
+                    BasalMeasure.Text = "mg";
+                    BolusMeasure.Text = "mg";
+                }
             }
         }
     }
