3f6303d822a4d51c3a509f7936394f74e343f8a2
Joshua Eudy <josh@familypharmacy.us>
2019-05-27 20:27:53 -0400

    Added switches for mg and mcg. Add days supply stepper.

--- PCACalc/PCACalc/Views/ConcentrationPage.xaml.cs
+++ PCACalc/PCACalc/Views/ConcentrationPage.xaml.cs
@@ -7,12 +7,14 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using PCACalc;
+using System.Diagnostics;
 
 namespace PCACalc.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConcentrationPage : ContentPage
     {
+        private string unitOfMeasure = "mg";
         public ConcentrationPage()
         {
             InitializeComponent();
@@ -22,23 +24,42 @@ namespace PCACalc.Views
         {
             double totalPerHour;
             double totalPerDay;
-            double totalPer5Day;
+            double totalPerDS;
             double bagVolume;
             double suggestedConcentration;
 
             totalPerHour = Calculations.CalcTPH(double.Parse(BasalEntry.Text), double.Parse(BolusEntry.Text), double.Parse(BolusMinEntry.Text));
-            TPHEntry.Text = totalPerHour.ToString();
+            TPHEntry.Text = totalPerHour.ToString() + unitOfMeasure;
 
             totalPerDay = totalPerHour * 24;
-            TPDEntry.Text = totalPerDay.ToString();
+            TPDEntry.Text = totalPerDay.ToString() + unitOfMeasure;
 
-            totalPer5Day = totalPerDay * 5;
-            TP5DEntry.Text = totalPer5Day.ToString();
+            totalPerDS = totalPerDay * StepDays.Value;
+            TP5DEntry.Text = totalPerDS.ToString() + unitOfMeasure;
 
             bagVolume = double.Parse(BagVolumeEntry.Text);
-            suggestedConcentration = totalPer5Day / bagVolume;
+            suggestedConcentration = Math.Round(totalPerDS / bagVolume, 0);
 
-            SugConcEntry.Text = suggestedConcentration.ToString();
+
+            SugConcEntry.Text = suggestedConcentration.ToString() + unitOfMeasure;
+        }
+
+        private void selectUOM(object sender, ToggledEventArgs e)
+        {
+            if(SwitchMG.IsToggled == true)
+            {
+                SwitchMCG.IsToggled = false;
+                unitOfMeasure = "mg";
+                BasalMeasure.Text = "mg";
+                BolusMeasure.Text = "mg";
+            }
+            else
+            {
+                SwitchMCG.IsToggled = true;
+                unitOfMeasure = "mcg";
+                BasalMeasure.Text = "mcg";
+                BolusMeasure.Text = "mcg";
+            }
         }
     }
 }
